419d7bbb3fe5a66c58868b917223896dae12b626
AmreetKumarkhuntia <amreet.khuntia@juspay.in>
2024-10-16 23:13:07 +0530

    fix: clone-template file not getting deleted
    - clone-template file not getting deleted

--- scripts/setup.js
+++ scripts/setup.js
@@ -2,7 +2,19 @@
 
 import { execSync } from 'child_process';
 
+// ANSI escape codes for styling
+const colors = {
+    reset: '\x1b[0m',
+    green: '\x1b[32m',
+    red: '\x1b[31m',
+    cyan: '\x1b[36m',
+    yellow: '\x1b[33m',
+};
+
+const separator = `${colors.cyan}\n------------------------------\n${colors.reset}`;
+
 const runCommand = (command) => {
+    console.log(`${separator} \n`);
     try {
         console.log(`\x1b[36mRunning: ${command}\x1b[0m`);
         execSync(command, { stdio: 'inherit' });
@@ -11,6 +23,7 @@ const runCommand = (command) => {
         console.error(`\x1b[31m❌ Error running command: ${error.message}\x1b[0m`);
         process.exit(1);
     }
+    console.log(`${separator} \n`);
 };
 
 // Define commands as an array
